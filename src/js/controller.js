94a8f4bed890f755e0b11554d8d8d36009559402
Alan <alangradecki@wp.pl>
2021-06-30 19:49:18 +0200

    refactoring for MVC

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,3 +1,5 @@
+import * as modal from './model.js';
+
 // import icons from '../img/icons.svg'; // Parcel 1
 import icons from 'url:../img/icons.svg'; // Parcel 2
 import 'core-js/stable';
@@ -33,32 +35,11 @@ const showRecipe = async function () {
     const id = window.location.hash.slice(1);
 
     if (!id) return;
-
-    // 1) Loading recipe
     renderSpinner(recipeContainer);
 
-    const res = await fetch(
-      // 'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bcc40'
-      `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
-    );
-
-    const data = await res.json();
-
-    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
-
-    let { recipe } = data.data;
-    recipe = {
-      id: recipe.id,
-      title: recipe.title,
-      publisher: recipe.publisher,
-      sourceUrl: recipe.source_url,
-      image: recipe.image_url,
-      servings: recipe.servings,
-      cookingTime: recipe.cooking_time,
-      ingredients: recipe.ingredients,
-    };
-
-    console.log(recipe);
+    // 1) Loading recipe
+    await modal.loadRecipe(id);
+    const { recipe } = modal.state;
 
     // 2) Rendering recipe
     const markup = `

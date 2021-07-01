b91981a0774e8e422b7ea3c6f35bbe44e64789d3
Alan <alangradecki@wp.pl>
2021-07-01 15:40:49 +0200

    implemented search result methods

--- src/js/controller.js
+++ src/js/controller.js
@@ -26,6 +26,17 @@ const controlRecipes = async function () {
   }
 };
 
+const controlSearchResults = async function () {
+  try {
+    await model.loadSearchResults('pizza');
+    console.log(model.state.search.results);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+controlSearchResults();
+
 const init = function () {
   recipeView.addHandlerRender(controlRecipes);
 };

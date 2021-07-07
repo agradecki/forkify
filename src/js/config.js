d71090867f3bb7556c6928d81f0cb8de44505990
Alan <alangradecki@wp.pl>
2021-07-07 11:27:41 +0200

    added some constans variables

--- src/js/config.js
+++ src/js/config.js
@@ -1,3 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
+export const KEY = '22847418-3d37-447d-a34f-26735fd15b2a';
+export const MODAL_CLOSE_SEC = 2.5;
